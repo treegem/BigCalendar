b98ee09397ac80c0079374fafd63aeb89e74bb17
h4ckamuenster <h4ckamuenster@web.de>
2018-03-05 00:18:26 +0100

    package installed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='big_calendar',
-    packages=['big_calendar'],
+    packages=['BigCalendar'],
     include_package_data=True,
     install_requires=[
         'flask',
