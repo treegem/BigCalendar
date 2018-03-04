fa0d4794bd4bbc2a9d02e76028bbcba6cd5260fa
h4ckamuenster <h4ckamuenster@web.de>
2018-03-05 00:14:35 +0100

    prepared for packaging

---
+++ BigCalendar/__init__.py
@@ -0,0 +1,1 @@
+from .big_calendar import app
--- BigCalendar/big_calendar.py
+++ BigCalendar/big_calendar.py
@@ -12,3 +12,10 @@ app.config.update(dict(
     USERNAME='admin',  # TODO
     PASSWORD='default'  # TODO
 ))
+
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
